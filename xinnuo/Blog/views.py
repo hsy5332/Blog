8bc2003f56d5bb566600010837b204d40a749a06
hsy5332 <allenyao224@qq.com>
2017-11-16 23:47:12 +0800

    忘记密码接口

--- xinnuo/Blog/views.py
+++ xinnuo/Blog/views.py
@@ -1,7 +1,7 @@
 import time
 import json
 import pdb
-#pdb.set_trace()
+# pdb.set_trace()
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -36,6 +36,7 @@ code = 206 请输入正确的手机号
 code = 207 没有获取到邮箱验证码，请重试。
 code = 208 请输入正确的邮箱地址！
 code = 209 请输入邮箱或者手机号。
+code = 210 用户名和手机号不匹配
 '''
 
 
@@ -44,6 +45,8 @@ def index(request):
 
     return render(request, 'index.html', {"TEST": user})
 
+
+# 登录接口
 
 def login(request):
     if request.POST:
@@ -77,7 +80,7 @@ def login(request):
         return HttpResponse(json.dumps(login_requesterror))
 
 
-# 登录接口
+# 注册接口
 
 def register(request):
     # request.encoding='utf-8'
@@ -118,12 +121,12 @@ def register(request):
                                     phonenumber=request.POST['phonenumber'],
                                     mcodestatus=1, usein='R')  # 判断phoneMessage是否有短信验证码
                                 register_messagecode = {'code': '200', 'msg': 'Success！',
-                                                            'data': phoneMessage.messagecode}
+                                                        'data': phoneMessage.messagecode}
                                 return HttpResponse(json.dumps(register_messagecode))
                             except:
                                 register_notReceivedcode = {'code': '204', 'msg': '没有获取到手机验证码，请重试。',
                                                             'data': ''}  # Receive收到
-                                return HttpResponse (json.dumps(register_notReceivedcode))
+                                return HttpResponse(json.dumps(register_notReceivedcode))
                     else:
                         register_Phonewrongful = {'code': '206', 'msg': '请输入正确的手机号！', 'data': ''}  # wrongful不合法
                         return HttpResponse(json.dumps(register_Phonewrongful))
@@ -131,7 +134,8 @@ def register(request):
                     try:
                         if "@" in request.POST['email']:
                             try:
-                                eMail = models.mail.objects.get(email=request.POST['email'], ecodestatus=1 ,usein = 'R')  # 判断是否有验证码
+                                eMail = models.mail.objects.get(email=request.POST['email'], ecodestatus=1,
+                                                                usein='R')  # 判断是否有验证码
                                 register_mailcode = {'code': '200', 'msg': 'Success', 'data': eMail.emailcode}
                                 return HttpResponse(json.dumps(register_mailcode))
                             except:
@@ -150,13 +154,57 @@ def register(request):
         return HttpResponse(json.dumps(register_requesterror))
 
 
-# 注册接口
-'''
+# 忘记密码接口
+
 def forgotpassword(request):
     if request.POST:
-        try:
-            forgotusername = models.user.objects.get(username=request.POST['username'],)
+        if 'phonenumber' in request.POST:
+            if len(request.POST['phonenumber']) >= 11:  # 判断手机号是否合法
+                try:
+                    forgotusername = models.user.objects.get(username=request.POST['username'],
+                                                             phonenumber=request.POST['phonenumber'])  # 判断用户名和手机是否匹配
+                    try:
+                        forgotphonecode = models.phone_message.objects.get(phonenumber=request.POST['phonenumber'],
+                                                                           messagecode=request.POST['messagecode'],
+                                                                           mcodestatus='1',
+                                                                           usein='F')  # 判断是否phone_message有验证码
+                        # forgotuserinfo = models.user.objects.get(username=request.POST['username'])
+                        forgotpassword_success = {'code': '200', 'msg': 'Success!',
+                                                  'data': ''}  # 后期可考虑返回一个token，前端进入修改密码页面时可用这个，或者返回用户名和密码。
+                        return HttpResponse(json.dumps(forgotpassword_success))
+                    except:
+                        forgotpassword_userNotmatching = {'code': '204', 'msg': '没有获取到手机验证码，请重试。', 'data': ''}
+                        return HttpResponse(json.dumps(forgotpassword_userNotmatching))
+                except:
+                    forgotpassword_userNotmatching = {'code': '201', 'msg': '用户名和手机号不匹配！', 'data': ''}  # 用户名和密码不匹配
+                    return HttpResponse(json.dumps(forgotpassword_userNotmatching))
+            else:
+                forgotpassword_Phonewrongful = {'code': '206', 'msg': '请输入正确的手机号！', 'data': ''}
+                return HttpResponse(json.dumps(forgotpassword_Phonewrongful))
 
-        except:
-            print("用户错误")
-'''
+        elif 'email' in request.POST:  # 判断邮箱是否合法
+            if '@' in request.POST:
+                try:
+                    forgotpassword_email = models.user.objects.get(username=request.POST['username'],
+                                                                   emailaddress=request.POST['email'])  # 判断用户名和邮箱地址是否匹配
+                    try:
+                        forgotpassword_emailcode = models.mail.objects.get(email=request.POST['email'],
+                                                                           emailcode=request.POST['emailcode'],
+                                                                           ecodestatus=1, usein='F')
+                        # forgotuserinfo = models.user.objects.get(username=request.POST['username'])
+                        forgotpassword_success = {'code': '200', 'msg': 'Success!',
+                                                  'data': ''}  # 后期可考虑返回一个token，前端进入修改密码页面时可用这个，或者返回用户名和密码。
+                        return HttpResponse(json.dumps(forgotpassword_success))
+                    except:
+                        forgotpassword_userNotmatching = {'code': '207', 'msg': '没有获取到邮箱验证码，请重试。', 'data': ''}
+                except:
+                    forgotpassword_mailwrongful = {'code': '211', 'msg': '用户名和邮箱地址不匹配！', 'data': ''}
+                    return HttpResponse(json.dumps(forgotpassword_mailwrongful))
+            else:
+                forgotpassword_mailerror = {'code': '208', 'msg': '请输入正确的邮箱地址！', 'data': ''}
+                return HttpResponse(json.dumps(forgotpassword_mailerror))
+        else:
+            forgotpassword_requestwrong = {'code': '-12', 'msg': '请求方式错误', 'data': ''}
+            return HttpResponse(json.dumps(forgotpassword_requestwrong))
+
+# 验证码获取接口
