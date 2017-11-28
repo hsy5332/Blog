e934a0b8917dbeeae72b7ed04541dd2fb73a7a2a
steel <allenyao224@qq.com>
2017-11-28 09:52:06 +0800

    models提交

--- xinnuo/Blog/models.py
+++ xinnuo/Blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-
+#article table 文章表
 class article(models.Model):
     articleid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='articleid')
-    userid = models.CharField(max_length=11,null=False)
+    authorid = models.CharField(max_length=11,null=False)
     classifyid =models.CharField(max_length=11,null=False)
     articletitle = models.TextField(null=False)
     articlecontent = models.TextField(null=False)
@@ -12,11 +12,11 @@ class article(models.Model):
     createdtime = models.DateTimeField(default='auto_now_add')
     updatetime = models.DateTimeField(default='auto_now')
 
-    def  __str__(self):
-        return self.articletitle #Python方法，调取函数，返回对象为articletitle，这样页面不会显示object
+    #def  __str__(self):
+        #return self.articletitle #Python方法，调取函数，返回对象为articletitle，这样页面不会显示object
 
-#article table 文章表
 
+#article_classify 文章分类表
 class article_classify(models.Model):
     classifyid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='classifyid')
     classifyname = models.CharField(max_length=11)
@@ -26,8 +26,8 @@ class article_classify(models.Model):
     def __str__(self):
         return self.classifyname
 
-#article_classify 文章分类表
 
+#collection_record 收藏记录表
 class collection_record(models.Model):
     collectionid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='collectionid')
     collentionUserid = models.CharField(max_length=11)
@@ -36,8 +36,8 @@ class collection_record(models.Model):
     createdtime = models.DateTimeField(default='auto_now_add')
     updatetime = models.DateTimeField(default='auto_now')
 
-#collection_record 收藏记录表
 
+#comment 评论表
 class comment(models.Model):
     commentid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='commentid')
     createrid = models.CharField(max_length=11)
@@ -47,8 +47,8 @@ class comment(models.Model):
     createdtime = models.DateTimeField(default='auto_now_add')
     updatetime = models.DateTimeField(default='auto_now')
 
-#comment 评论表
 
+#count 统计表
 class count(models.Model):
     countid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='countid')
     articleid = models.CharField(max_length=11)
@@ -56,8 +56,8 @@ class count(models.Model):
     createdtime = models.DateTimeField(default='auto_now_add')
     updatetime = models.DateTimeField(default='auto_now')
 
-#count 统计表
 
+#leaving_message 留言内容表
 class leaving_message(models.Model):
     leavingid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='leavingid')
     createrid = models.CharField(max_length=11)
@@ -67,47 +67,62 @@ class leaving_message(models.Model):
     createdtime = models.DateTimeField(default='auto_now_add')
     updatetime = models.DateTimeField(default='auto_now')
 
-#leaving_message 留言内容表
 
+#mail 邮箱验证表
 class mail(models.Model):
     emailid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='emailid')
     email = models.CharField(max_length=50)
     emailcode = models.CharField(max_length=20)
     ecodestatus = models.CharField(max_length=4)
+    usein = models.CharField(max_length=4)
     createdtime = models.DateTimeField(default='auto_now_add')
     updatetime = models.DateTimeField(default='auto_now')
 
-#mail 邮箱验证表
 
+#phone_message 手机短信表
 class phone_message(models.Model):
     messageid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='messageid')
     phonenumber = models.CharField(max_length=14)
     messagecode = models.CharField(max_length=20)
+    mcodestatus = models.CharField(max_length=4)
+    usein = models.CharField(max_length=4)
     createdtime = models.DateTimeField(default='auto_now_add')
     updatetime = models.DateTimeField(default='auto_now')
+    def __int__(self):
+        return int.phonenumber
 
-#phone_message 手机短信表
-
+#phone_status手机状态表
 class phone_status(models.Model):
     phoneid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='phoneid')
     phonenumber = models.CharField(max_length=14)
     status = models.CharField(max_length=4)
 
-#phone_status手机状态表
 
+#user  用户信息表
 class user(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='id')
-    username = models.CharField(max_length=20)
+    username = models.CharField(max_length=40)
     password = models.CharField(max_length=50)
-    realname = models.CharField(max_length=30)
-    nickname = models.CharField(max_length=30)
+    realname = models.CharField(max_length=40)
+    nickname = models.CharField(max_length=40)
     phonenumber = models.CharField(max_length=14)
     emailaddress = models.CharField(max_length=50)
     head = models.CharField(max_length=50)
     birthday = models.DateTimeField(default='auto_now_add')
     useraddress = models.CharField(max_length=100)
+    sex = models.CharField(max_length=4)
     userstatus = models.CharField(max_length=4)
     createdtime = models.DateTimeField(default='auto_now_add')
     updatetime = models.DateTimeField(default='auto_now')
 
-#user  用户信息表
+
+#主页配置
+class link_configure(models.Model):
+    linkid = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='linkid')
+    url = models.CharField(max_length=50)
+    picture = models.CharField(max_length=50)
+    configurestatus = models.CharField(max_length=4)
+    position = models.CharField(max_length=4)
+    number = models.CharField(max_length=4)
+    createdtime = models.DateTimeField(default='auto_now_add')
+    updatetime = models.DateTimeField(default='auto_now')
