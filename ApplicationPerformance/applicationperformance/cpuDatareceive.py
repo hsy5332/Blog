8409008e7335f4e22c872a72c7b4257ef89110ee
steel <allenyao224@qq.com>
2017-12-18 20:46:18 +0800

    增加cpu，修改launchtime

--- ApplicationPerformance/applicationperformance/cpuDatareceive.py
+++ ApplicationPerformance/applicationperformance/cpuDatareceive.py
@@ -1,17 +1,97 @@
-import ApplicationPerformance.applicationperformance.launchTime
+import applicationperformance.launchTime
+import os
+import time
 
 
 class CpuApplicationData(object):
-    #执行monkey脚本
-    def monkeyRun(self):
-        pass
+    def receiveCpuDataCmd(self, packname, devicesid):
+        if devicesid != "":
+            receivecpucmd = "adb -s %s shell dumpsys cpuinfo | grep %s" % (devicesid, packname)
+            receivecpudata = os.popen(receivecpucmd)
+            return receivecpudata
+        else:
+            receivecpucmd = "adb shell dumpsys cpuinfo | grep %s" % (packname)
+            receivecpudata = os.popen(receivecpucmd)
+            return receivecpudata
 
-    #获取CPU数据且保存数据
+    # 执行monkey脚本
+    def monkeyRun(self, monkeyscript):
+        monkeycmd = os.popen(monkeyscript)
+
+
+    # 获取CPU数据且保存数据
     def receiveCpuData(self):
-        caserows = ApplicationPerformance.applicationperformance.launchTime.ReadExcel().readeExcelData('cpudata')
-        for i in  range(1,caserows.get('caserows')):
-            print(i)
+        caserows = applicationperformance.launchTime.ReadExcel().readeExcelData('cpudata')
+        for i in range(1, caserows.get('caserows')):
+            casedata = caserows.get('excledata_sheel').row_values(i)
+            caseid = int(casedata[0])
+            packname = casedata[1]
+            monkeyscript = casedata[2]
+            functionscript = casedata[3]
+            count = int(casedata[4])
+            intervaltime = int(casedata[5])
+            devicesid = casedata[6]
+            returndata = caseid, packname, monkeyscript, functionscript, count, intervaltime, devicesid
+            if count > 0:
+                print("执行用例编号:%s，用例数据为：%s" % (caseid, returndata))
+                if monkeyscript != "":
+                    startruntime = time.time()
+                    counts = 1
+                    while count > 0:
+                        CpuApplicationData().monkeyRun(monkeyscript)
+                        startruntime = time.time()
+                        receivecpudata = CpuApplicationData().receiveCpuDataCmd(packname, devicesid)
+                        for cpudatas in receivecpudata:
+                            cpudatas = cpudatas.split("%")
+                            cpuproportion = cpudatas[0].strip() + "%"
+                        endruntime = time.time()
+                        runtime = int(str(endruntime - startruntime).split(".")[1][0:4])
+                        runtimes = endruntime - startruntime
+                        savedata = "insert into automation_cpu_app  (`cpuproportion`,`starttime`,`endtime`,`monkeyscript`,`functionscript`,`createdtime`,`updatetime`,`caseid`,`runtime`)VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (
+                            cpuproportion, startruntime, endruntime, monkeyscript, functionscript,
+                            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), caseid, runtimes)
+                        applicationperformance.launchTime.MysqlConnect().saveDatatoMysql("%s" % (savedata))
+                        print("用例编号：%s,第%s次执行，执行时间为：%s ms。" % (caseid, counts, runtime))
+                        time.sleep(intervaltime)
+                        count -= 1
+                        counts += 1
+                elif functionscript != "":
+                    # 执行自动化测试用例的脚本
+                    startruntime = time.time()
+                    while count > 0:
+                        receivecpudata = CpuApplicationData().receiveCpuDataCmd(packname, devicesid)
+                        for cpudatas in receivecpudata:
+                            cpudatas = cpudatas.split("%")
+                            cpudata = cpudatas[0].strip() + "%"
+                            print(cpudata)
+                        time.sleep(intervaltime)
+                        count -= 1
+                    print("我是functionstatus")
+                    endruntime = time.time()
+                else:
+                    counts  = 1
+                    while count > 0:
+                        startruntime = time.time()
+                        receivecpudata = CpuApplicationData().receiveCpuDataCmd(packname, devicesid)
+                        for cpudatas in receivecpudata:
+                            cpudatas = cpudatas.split("%")
+                            cpuproportion = cpudatas[0].strip() + "%"
+                        endruntime = time.time()
+                        runtime = int(str(endruntime - startruntime).split(".")[1][0:4])
+                        runtimes = endruntime - startruntime
+                        savedata = "insert into automation_cpu_app  (`cpuproportion`,`starttime`,`endtime`,`monkeyscript`,`functionscript`,`createdtime`,`updatetime`,`caseid`,`runtime`)VALUES('%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (
+                            cpuproportion, startruntime, endruntime, monkeyscript, functionscript,
+                            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), caseid, runtimes)
+                        applicationperformance.launchTime.MysqlConnect().saveDatatoMysql("%s" % (savedata))
+                        print("用例编号：%s,第%s次执行，执行时间为：%s ms。" % (caseid,counts, runtime))
+                        time.sleep(intervaltime)
+                        count -= 1
+                        counts += 1
+            else:
+                print("用例编号:%s，该用例不执行，用例数据为：%s" % (caseid, returndata))
 
 
-
-CpuApplicationData().receiveCpuData()
+if __name__ == "__main__":
+    CpuApplicationData().receiveCpuData()
--- ApplicationPerformance/applicationperformance/launchTime.py
+++ ApplicationPerformance/applicationperformance/launchTime.py
@@ -6,21 +6,44 @@ import xlrd
 
 class LaunchApplication(object):
     # 冷启动APP
-    def coolLaunch(self, packactivity):
-        launchcmd = 'adb shell am start -W -n %s' % (packactivity)
-        launchtcmddata = os.popen(launchcmd)
-        return launchtcmddata
+    def coolLaunch(self, packactivity, devicesid):
+        if devicesid != "":
+            launchcmd = 'adb -s %s shell am start -W -n %s' % (devicesid, packactivity)
+            launchtcmddata = os.popen(launchcmd)
+            return launchtcmddata
+        else:
+            launchcmd = 'adb shell am start -W -n %s' % (packactivity)
+            launchtcmddata = os.popen(launchcmd)
+            return launchtcmddata
 
     # 停止APP
-    def stopApp(self, packname):
-        stopcmd = 'adb shell am force-stop %s' % (packname)
-        stopcmddata = os.popen(stopcmd)
-        return stopcmddata
+    def stopApp(self, packname, devicesid):
+        if devicesid != "":
+            stopcmd = 'adb -s %s shell am force-stop %s' % (devicesid, packname)
+            stopcmddata = os.popen(stopcmd)
+            return stopcmddata
+        else:
+            stopcmd = 'adb shell am force-stop %s' % (packname)
+            stopcmddata = os.popen(stopcmd)
+            return stopcmddata
 
     # 热启动
-    def hotLaunch(self):
-        hotLaunchcmd = 'adb shell input keyevent 3'
-        hotLaunchcmddata = os.popen(hotLaunchcmd)
+    def hotLaunch(self,devicesid):
+        if devicesid != "":
+            hotLaunchcmd = 'adb -s %s shell input keyevent 3'%(devicesid)
+            hotLaunchcmddata = os.popen(hotLaunchcmd)
+        else:
+            hotLaunchcmd = 'adb shell input keyevent 3'
+            hotLaunchcmddata = os.popen(hotLaunchcmd)
+    #获取设备ID 暂时不用
+    def receiveDevices(self):
+        devicesid = os.popen("adb devices")
+        devices = []
+        for x in devicesid:
+            if 'attached' not in x:
+                x = x.strip('\r\n')
+                devices.append(x)
+        return devices
 
     # 运行且保存数据
     def runApp(self):
@@ -34,21 +57,22 @@ class LaunchApplication(object):
                 packname = casedata[2]
                 launchtype = casedata[3]
                 count = int(casedata[4])
-                returndata = caseid, packactivity, packname, launchtype, count
+                devicesid = casedata[5]
+                returndata = caseid, packactivity, packname, launchtype, count, devicesid
                 if count > 0:
                     print("执行用例编号:%s，用例数据为：%s" % (caseid, returndata))
                     if launchtype == 'C':
                         launchcount = 1  # 记录启动次数
                         while count > 0:
                             starttime = time.time()
-                            launchtcmddata = LaunchApplication().coolLaunch(packactivity)  # 热启动APP
+                            launchtcmddata = LaunchApplication().coolLaunch(packactivity, devicesid)  # 热启动APP
                             for launchtimes in launchtcmddata:
                                 if "ThisTime" in launchtimes:
                                     launchtime = launchtimes.split(':')[1].strip() + 'ms'
                                     endtime = time.time()
                                     print("第%s次启动时间为：" % (launchcount), launchtime)
                                     time.sleep(5)
-                                    LaunchApplication().stopApp(packname)  # 关闭APP
+                                    LaunchApplication().stopApp(packname, devicesid)  # 关闭APP
                                     savedata = "insert into automation_Launch_app  (`starttime`,`launchtime`,`endtime`,`launchtype`,`createdtime`,`updatetime`,`caseid`)VALUES('%s','%s','%s','%s','%s','%s','%s')" % (
                                         starttime, launchtime, endtime, launchtype,
                                         time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
@@ -61,18 +85,18 @@ class LaunchApplication(object):
                     elif launchtype == 'H':  # 热启动方法，冷启动-back-热启动
                         launchcount = 1  # 记录启动次数
                         while count > 0:
-                            LaunchApplication().coolLaunch(packactivity)  # 热启动APP
+                            LaunchApplication().coolLaunch(packactivity, devicesid)  # 冷启动APP
                             time.sleep(5)
                             starttime = time.time()
-                            LaunchApplication().hotLaunch()  # back
-                            launchtcmddata = LaunchApplication().coolLaunch(packactivity)  # 热启动APP
+                            LaunchApplication().hotLaunch(devicesid)  # back
+                            launchtcmddata = LaunchApplication().coolLaunch(packactivity, devicesid)  # 热启动APP
                             for launchtimes in launchtcmddata:
                                 if "ThisTime" in launchtimes:
                                     launchtime = launchtimes.split(':')[1].strip() + 'ms'
                                     endtime = time.time()
                                     print(launchtime)
                                     time.sleep(5)
-                                    LaunchApplication().stopApp(packname)  # 关闭APP
+                                    LaunchApplication().stopApp(packname, devicesid)  # 关闭APP
                                     savedata = "insert into automation_Launch_app  (`starttime`,`launchtime`,`endtime`,`launchtype`,`createdtime`,`updatetime`,`caseid`)VALUES('%s','%s','%s','%s','%s','%s','%s')" % (
                                         starttime, launchtime, endtime, launchtype,
                                         time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
@@ -85,33 +109,32 @@ class LaunchApplication(object):
                     else:
                         print("您的启动类型有错，请检查您的启动类型")
                 else:
-                    print("用例编号%s为0，不执行该用例。" % (caseid))
-                    print("执行用例编号:%s，用例数据为：%s" % (caseid, returndata))
-
+                    print("用例编号:%s，该用例不执行，用例数据为：%s" % (caseid, returndata))
         except:
             print("请检查您的用例，查看是否有错")
         endtimecasetime = time.time()
         print("执行用例时间为:%ss" % (round(endtimecasetime - startcasetime, 2)))
 
-#数据库连接和保存测试结果
+
+# 数据库连接和保存测试结果
 class MysqlConnect(object):
     def queryDatatoMysql(self, sql):  # 查询数据
-        # connect = pymysql.connect(
-        #     host='steel.iask.in',
-        #     port=33067,
-        #     user='huangshunyao',
-        #     password='Hsy5332#',
-        #     db='automation_db',
-        #     charset = 'utf8', #解决中文乱码
-        # )
         connect = pymysql.connect(
-            host='192.168.1.9',  # 测试环境
-            port=33006,
+            host='steel.iask.in',
+            port=33067,
             user='huangshunyao',
-            passwd='Hsy5332#',  # 注意password
+            password='Hsy5332#',
             db='automation_db',
             charset='utf8',  # 解决中文乱码
         )
+        # connect = pymysql.connect(
+        #     host='192.168.1.9',  # 测试环境
+        #     port=33006,
+        #     user='huangshunyao',
+        #     passwd='Hsy5332#',  # 注意password
+        #     db='automation_db',
+        #     charset='utf8',  # 解决中文乱码
+        # )
 
         cursor = connect.cursor()  # 获取游标
         cursor.execute(sql)  # 执行sql语句
@@ -120,15 +143,22 @@ class MysqlConnect(object):
         return data
 
     def saveDatatoMysql(self, sql):  # 保存数据
+        # connect = pymysql.connect(
+        #     host='192.168.1.9',  # 测试环境
+        #     port=33006,
+        #     user='huangshunyao',
+        #     passwd='Hsy5332#',  # 注意password
+        #     db='automation_db',
+        #     charset='utf8',  # 解决中文乱码
+        # )
         connect = pymysql.connect(
-            host='192.168.1.9',  # 测试环境
-            port=33006,
+            host='steel.iask.in',
+            port=33067,
             user='huangshunyao',
-            passwd='Hsy5332#',  # 注意password
+            password='Hsy5332#',
             db='automation_db',
             charset='utf8',  # 解决中文乱码
         )
-
         cursor = connect.cursor()  # 获取游标
         cursor.execute(sql)  # 执行sql语句
         connect.commit()  # 提交数据
@@ -137,7 +167,7 @@ class MysqlConnect(object):
 
 # 读取用例
 class ReadExcel(object):
-    def readeExcelData(self,launchtime):
+    def readeExcelData(self, launchtime):
         if launchtime == 'launchtime':
             excelcase = xlrd.open_workbook("one.xlsx")
             excledata_sheel = excelcase.sheet_by_name('launchtime')
@@ -150,6 +180,7 @@ class ReadExcel(object):
             caserows = excledata_sheel.nrows
             returndata = {'caserows': caserows, 'excledata_sheel': excledata_sheel}
             return returndata
+
 
 if __name__ == "__main__":
     LaunchApplication().runApp()
