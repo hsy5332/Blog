0d4e1bad95e0305a6d5235626b4263069e23d9d1
steel <allenyao224@qq.com>
2017-12-27 18:45:14 +0800

    functionautomation

--- ApplicationPerformance/applicationfunction/functionAutomation.py
+++ ApplicationPerformance/applicationfunction/functionAutomation.py
@@ -1,54 +1,107 @@
 import applicationperformance.launchTime as launchTime
 from appium import webdriver
 import time
-class FunctionAutomation(object):
 
-    #初始化设备，读取设备信息
-    def originalDevices(self,deviceName,platformName,platformVersion,appPackage,appActivity):
+
+class FunctionAutomation(object):
+    # 初始化设备，读取设备信息
+    def originalDevices(self, deviceName, platformName, platformVersion, appPackage, appActivity, udid):
         devicesinfo = {}
         devicesinfo['deviceName'] = deviceName
         devicesinfo['platformName'] = platformName
         devicesinfo['platformVersion'] = platformVersion
         devicesinfo['appPackage'] = appPackage
         devicesinfo['appActivity'] = appActivity
+        devicesinfo['udid'] = udid
         return devicesinfo
 
-    #连接设备
+    # 连接设备
     def connectDevices(self):
         pass
 
-    #断开设备连接
+    # 断开设备连接
     def stopConnectDevices(self):
         pass
 
-    #点击事件
-    def operateClick(self):
+    # 点击事件
+    def operateClick(self, operatetype, element, driver, caseid):
+
+        if operatetype == "点击_id":
+            try:
+                driver.find_element_by_id(element).click()  # 点击ID事件
+                print("用例编号:%s,执行通过。" % (caseid))
+            except:
+                print("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "点击_xpath":
+            try:
+                driver.find_element_by_xpath(element).click()  # 点击xpath
+                print("用例编号:%s,执行通过。" % (caseid))
+            except:
+                print("用例编号:%s,执行不通过。" % (caseid))
+
+    # 滑动操作
+    def operateslide(self, direction):
         pass
 
-    #滑动操作
-    def operateslide(self,direction):
-        pass
-
-    #长按操作
+    # 长按操作
     def operateLongClick(self):
         pass
 
-    #运行用例
+    # 运行用例
     def runTestCase(self):
         deviceinfo = launchTime.ReadExcel().readeExcelData('deviceinfo')
         for i in range(1, deviceinfo.get('caserows')):
-            casedata = deviceinfo.get('excledata_sheel').row_values(i)
-            print(casedata)
-            deviceName = casedata[0]
-            platformName= casedata[1]
-            platformVersion = casedata[2]
-            appPackage = casedata[3]
-            appActivity = casedata[4]
-            port = int(casedata[5])
-            devicesinfo =FunctionAutomation().originalDevices(deviceName,platformName,platformVersion,appPackage,appActivity)
-            driver = webdriver.Remote('http://localhost:%s/wd/hub' % (port), devicesinfo)
-            time.sleep(10)
-            driver.quit()
+            devicescase = deviceinfo.get('excledata_sheel').row_values(i)
+            deviceName = devicescase[0]  # 设备名称
+            udid = devicescase[0]  # 设备uid
+            platformName = devicescase[1]  # 系统名称
+            platformVersion = devicescase[2]  # 系统版本
+            appPackage = devicescase[3]  # 测试APP的包名
+            appActivity = devicescase[4]  # 测试AppActivity
+            port = int(devicescase[5])  # 连接Appium端口号
+            devicesinfo = FunctionAutomation().originalDevices(deviceName, platformName, platformVersion,
+                                                               appPackage, appActivity, udid)  # 连接测试设备
+            casedata = launchTime.ReadExcel().readeExcelData('funcase')  # 读取自动化用例数据
+            if platformName == "Android":
+                try:
+                    driver = webdriver.Remote('http://localhost:%s/wd/hub' % (port), devicesinfo)  # 连接Appium
+                    time.sleep(3)
+                    startcasetime = time.time()  # 开始执行用例时间
+                    for x in range(1, casedata.get('caserows')):  # Excel中的测试用例数据，使用for遍历每一行的数据，进行判断执行对应的操作
+                        excelcasedata = casedata.get('excledata_sheel').row_values(
+                            x)
+                        caseid = int(excelcasedata[0])  # 用例编号
+                        operatetype = excelcasedata[1]  # 操作类型
+                        element = excelcasedata[2]  # 元素属性
+                        parameter = excelcasedata[3]  # 参数（如：输入的数据）
+                        checkpoint = excelcasedata[4]  # 检查点对比
+                        if excelcasedata[5] == "":
+                            waittime = 2
+                        else:
+                            waittime = int(excelcasedata[5])  # 等待时间
+                        if "Y" in excelcasedata[7]:
+                            if operatetype == "点击_id":  # 根据ID点击
+                                FunctionAutomation().operateClick(operatetype, element, driver, caseid)
+                                time.sleep(waittime)
+                            elif operatetype == "点击_xpath":  # 根据xpath点击
+                                FunctionAutomation().operateClick(operatetype, element, driver, caseid)
+                                time.sleep(waittime)
+                            elif operatetype == "test":
+                                pass
+                            else:
+                                print("用例编号:%s操作类型错误,该用例不执行。" % (caseid))
+                        else:
+                            print("用例编号:%s,执行状态为No,故不执行。" % (caseid))
+                    driver.quit()  # 关闭Appium的连接
+                    endtimecasetime = time.time()  # 结束用例执行时间
+                    print("执行用例时间为:%ss" % (round(endtimecasetime - startcasetime, 2)))  # 计算执行用例运行时间
+                except:
+                    driver.quit()
+                    print("请检查Appium和设备是否正常连接。")
+            elif platformName == "IOS":
+                pass
+            else:
+                print("当前系统名称输入错误，请输入正确的设备系统，Android或IOS。")
 
 
 FunctionAutomation().runTestCase()
--- ApplicationPerformance/applicationfunction/testdemo.py
+++ ApplicationPerformance/applicationfunction/testdemo.py
@@ -1,16 +1,20 @@
 from appium import webdriver
 import time
+
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['platformVersion'] = '4.4.2'
 desired_caps['deviceName'] = 'emulator-5554'
-desired_caps['appPackage'] = 'com.yimi.student.mobile'
-desired_caps['appActivity'] = 'com.yimi.student.activity.WelcomeActivity'
+desired_caps['udid'] = 'emulator-5554'
+desired_caps['appPackage'] = 'com.ushaqi.zhuishushenqi'
+desired_caps['appActivity'] = 'com.ushaqi.zhuishushenqi.ui.SplashActivity'
 
-driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+driver = webdriver.Remote('http://localhost:4726/wd/hub', desired_caps)
 time.sleep(5)
 x = driver.get_window_size()['width']
 y = driver.get_window_size()['height']
 print(x,y)
-#driver.find_element_by_name("1").click()
+driver.find_element_by_id("com.ushaqi.zhuishushenqi:id/btnEntryApp").click()
+#driver.find_element_by_xpath("//android.widget.ImageView[@resource-id='com.ushaqi.zhuishushenqi:id/home_action_menu_more']").click()
+driver.find_element_by_name(u"去添加").click()
 driver.quit()
