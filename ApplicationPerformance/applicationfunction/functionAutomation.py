6341dcd360e87415d80fdc0d5c314509e0d38820
Androidapplication <allenyao224@qq.com>
2018-01-01 19:18:45 +0800

    功能测试完善

--- ApplicationPerformance/applicationfunction/functionAutomation.py
+++ ApplicationPerformance/applicationfunction/functionAutomation.py
@@ -1,7 +1,10 @@
-import applicationperformance.launchTime as launchTime
-from appium import webdriver
+# import applicationperformance.launchTime as launchTime #MAC
+import ApplicationPerformance.applicationperformance.launchTime as launchTime  # Windows
 import time
+
+from appium import webdriver
 from appium.webdriver.common.touch_action import TouchAction
+from selenium.common.exceptions import NoSuchElementException  # 导入selenium NoSuchElementException异常模块
 
 
 class FunctionAutomation(object):
@@ -14,15 +17,9 @@ class FunctionAutomation(object):
         devicesinfo['appPackage'] = appPackage
         devicesinfo['appActivity'] = appActivity
         devicesinfo['udid'] = udid
+        devicesinfo['unicodeKeyboard'] = "True"
+        devicesinfo['resetKeyboard'] = "True"
         return devicesinfo
-
-    # 连接设备
-    def connectDevices(self):
-        pass
-
-    # 断开设备连接
-    def stopConnectDevices(self):
-        pass
 
     # 点击事件
     def operateClick(self, operatetype, element, driver, caseid, *parameter):
@@ -39,9 +36,16 @@ class FunctionAutomation(object):
                 print("用例编号:%s,执行通过。" % (caseid))
             except:
                 print("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "点击_textname":  # 点击textname
+            try:
+                driver.find_elements_by_name(element)[0].click()
+                print("用例编号:%s,执行通过。" % (caseid))
+            except:
+                print("用例编号:%s,执行不通过。" % (caseid))
+
         elif operatetype == "点击_classid":
             try:
-                driver.find_elements_by_class_name(element)[parameter[0]].click()  # 点击xpath
+                driver.find_elements_by_class_name(element)[0].click()  # 点击xpath
                 print("用例编号:%s,执行通过。" % (caseid))
             except:
                 print("用例编号:%s,执行不通过。" % (caseid))
@@ -79,35 +83,133 @@ class FunctionAutomation(object):
         else:
             print("用例编号:%s,执行不通过，该用例的元素属性或参数可能有问题，请检查该用例。" % (caseid))
 
+    # 检查元素是否存在
+    def operateCheckelement(self, operatetype, element, driver, caseid):
+        if operatetype == "查找_id":
+            try:
+                driver.find_element_by_id(element)
+                print("用例编号:%s,执行通过。" % (caseid))
+            except NoSuchElementException:
+                print("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "查找_xpath":
+            try:
+                driver.find_element_by_xpath(element)
+                print("用例编号:%s,执行通过。" % (caseid))
+            except NoSuchElementException:
+                print("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "查找_classid":
+            try:
+                driver.find_elements_by_class_name(element)
+                print("用例编号:%s,执行通过。" % (caseid))
+            except NoSuchElementException:
+                print("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "查找_textname":
+            try:
+                driver.find_elements_by_name(element)[0]
+                print("用例编号:%s,执行通过。" % (caseid))
+            except NoSuchElementException:
+                print("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "if包含_id":
+            try:
+                driver.find_element_by_id(element)
+                return ("用例编号:%s,执行通过。" % (caseid))
+            except NoSuchElementException:
+                return ("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "if包含_xpath":
+            try:
+                driver.find_element_by_xpath(element)
+                return("用例编号:%s,执行通过。" % (caseid))
+            except NoSuchElementException:
+                return("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "if包含_classid":
+            try:
+                driver.find_elements_by_class_name(element)
+                return("用例编号:%s,执行通过。" % (caseid))
+            except NoSuchElementException:
+                return("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "if包含_textname":
+            try:
+                driver.find_elements_by_name(element)[0]
+                return("用例编号:%s,执行通过。" % (caseid))
+            except NoSuchElementException:
+                return("用例编号:%s,执行不通过。" % (caseid))
+        else:
+            return("用例编号:%s,执行不通过，该用例的元素属性可能有问题，请检查该用例。" % (caseid))
+
+    # 拖拽操作
+    def operateDrag(self):
+        pass
+
     # 长按操作
-    def operateLongClick(self, operatetype, element, driver, caseid,*parameter):
-        if operatetype == "长按_id":
+    def operateLongClick(self, operatetype, element, driver, caseid, *parameter):
+        if operatetype == "长按_id":  # 长按元素id
             try:
                 el = driver.find_element_by_id(element)
                 TouchAction(driver).long_press(el, 1).release().perform()
                 print("用例编号:%s,执行通过。" % (caseid))
             except:
                 print("用例编号:%s,执行不通过。" % (caseid))
-        elif operatetype == "长按_classid":
+        elif operatetype == "长按_classid":  # 长按元素classname
             try:
-                el = driver.find_elements_by_class_name[element][parameter[0]]
+                el = driver.find_elements_by_class_name(element)[0]
                 TouchAction(driver).long_press(el, 1).release().perform()
                 print("用例编号:%s,执行通过。" % (caseid))
             except:
                 print("用例编号:%s,执行不通过。" % (caseid))
-        elif operatetype == "长按_xpath":
+        elif operatetype == "长按_xpath":  # 长按元素xpath
             try:
                 el = driver.find_element_by_xpath(element)
-                TouchAction(driver).long_press(el,1).release().perform()
+                TouchAction(driver).long_press(el, 1).release().perform()
+                print("用例编号:%s,执行通过。" % (caseid))
+            except:
+                print("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "长按_textname":  # 长按textname
+            try:
+                el = driver.find_elements_by_name(element)[0]
+                TouchAction(driver).long_press(el, 1).release().perform()
                 print("用例编号:%s,执行通过。" % (caseid))
             except:
                 print("用例编号:%s,执行不通过。" % (caseid))
         else:
             print("用例编号:%s,执行不通过，该用例的元素属性或参数可能有问题，请检查该用例。" % (caseid))
 
-    #输入操作
-    def operateInput(self):
-        pass
+    # 输入操作
+    def operateInput(self, operatetype, element, driver, caseid, *parameter):
+        if operatetype == "输入_id":
+            try:
+                driver.find_element_by_id(element).send_keys(parameter[0])
+                print("用例编号:%s,执行通过。" % (caseid))
+            except:
+                print("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "输入_xpath":
+            try:
+                driver.find_element_by_xpath(element).send_keys(parameter[0])
+                print("用例编号:%s,执行通过。" % (caseid))
+            except:
+                print("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "输入_classid":
+            try:
+                driver.find_elements_by_class_name(element)[0].send_keys(parameter[0])
+                print("用例编号:%s,执行通过。" % (caseid))
+            except:
+                print("用例编号:%s,执行不通过。" % (caseid))
+        elif operatetype == "输入_textname":
+            try:
+                driver.find_elements_by_name(element)[0].send_keys(parameter[0])
+                print("用例编号:%s,执行通过。" % (caseid))
+            except:
+                print("用例编号:%s,执行不通过。" % (caseid))
+        else:
+            print("用例编号:%s,执行不通过，该用例的元素属性或参数可能有问题，请检查该用例。" % (caseid))
+
+    # Android物理按键操作
+    def operatePhysicsKye(self, driver, caseid, *parameter):
+        keycode = parameter[0]
+        try:
+            driver.keyevent(keycode)
+            print("用例编号:%s,执行通过。" % (caseid))
+        except:
+            print("用例编号:%s,执行不通过。" % (caseid))
 
     # 运行用例
     def runTestCase(self):
@@ -124,33 +226,50 @@ class FunctionAutomation(object):
             devicesinfo = FunctionAutomation().originalDevices(deviceName, platformName, platformVersion,
                                                                appPackage, appActivity, udid)  # 连接测试设备
             casedata = launchTime.ReadExcel().readeExcelData('funcase')  # 读取自动化用例数据
+            endcasenumber = []
+            casenumber = []
+            for j in range(1, casedata.get('caserows')):  # Excel中的测试用例数据，使用for遍历每一行的数据，进行判断执行对应的操作
+                excelcasedata = casedata.get('excledata_sheel').row_values(
+                    j)
+                operatetype = excelcasedata[1]
+                if "if" in operatetype:
+                    casenumber.append(j)
+                if "end" in operatetype:
+                    endcasenumber.append(j)
             if platformName == "Android":
                 try:
                     driver = webdriver.Remote('http://localhost:%s/wd/hub' % (port), devicesinfo)  # 连接Appium
-                    time.sleep(3)
                     startcasetime = time.time()  # 开始执行用例时间
-                    for x in range(1, casedata.get('caserows')):  # Excel中的测试用例数据，使用for遍历每一行的数据，进行判断执行对应的操作
+                    x = 1
+                    ifnumber = 0
+                    while x < casedata.get('caserows'):  # Excel中的测试用例数据，使用for遍历每一行的数据，进行判断执行对应的操作
                         excelcasedata = casedata.get('excledata_sheel').row_values(
                             x)
+                        x = x + 1
                         caseid = int(excelcasedata[0])  # 用例编号
                         operatetype = excelcasedata[1]  # 操作类型
                         element = excelcasedata[2]  # 元素属性
-                        parameter = excelcasedata[3]  # 参数（如：输入的数据）
+                        parameter = excelcasedata[3]  # 参数（如：输入的数据）x
                         checkpoint = excelcasedata[4]  # 检查点对比
                         if excelcasedata[5] == "":
                             waittime = 2
                         else:
                             waittime = int(excelcasedata[5])  # 等待时间
                         if "Y" in excelcasedata[7]:
-                            if operatetype == "点击_id":  # 根据ID点击
+                            if operatetype == "等待时间":
+                                time.sleep(waittime)
+                                print("用例编号:%s,执行通过。" % (caseid))
+                            elif operatetype == "点击_id":  # 根据ID点击
                                 FunctionAutomation().operateClick(operatetype, element, driver, caseid)
                                 time.sleep(waittime)
                             elif operatetype == "点击_xpath":  # 根据xpath点击
                                 FunctionAutomation().operateClick(operatetype, element, driver, caseid)
                                 time.sleep(waittime)
                             elif operatetype == "点击_classid":
-                                FunctionAutomation().operateClick(operatetype, element, driver, caseid, int(parameter))
+                                FunctionAutomation().operateClick(operatetype, element, driver, caseid)
                                 time.sleep(waittime)
+                            elif operatetype == "点击_textname":
+                                FunctionAutomation().operateClick(operatetype, element, driver, caseid)
                             elif operatetype == "向上滑动":
                                 FunctionAutomation().operateslide(operatetype, driver, caseid)
                                 time.sleep(waittime)
@@ -164,14 +283,113 @@ class FunctionAutomation(object):
                                 FunctionAutomation().operateslide(operatetype, driver, caseid)
                                 time.sleep(waittime)
                             elif operatetype == "长按_id":
-                                FunctionAutomation().operateLongClick(operatetype,element,driver,caseid)
+                                FunctionAutomation().operateLongClick(operatetype, element, driver, caseid)
                                 time.sleep(waittime)
                             elif operatetype == "长按_xpath":
                                 FunctionAutomation().operateLongClick(operatetype, element, driver, caseid)
                                 time.sleep(waittime)
                             elif operatetype == "长按_classid":
-                                FunctionAutomation().operateLongClick(operatetype, element, driver, caseid, int(parameter))
+                                FunctionAutomation().operateLongClick(operatetype, element, driver, caseid)
                                 time.sleep(waittime)
+                            elif operatetype == "长按_textname":
+                                FunctionAutomation().operateLongClick(operatetype, element, driver, caseid)
+                                time.sleep(waittime)
+                            elif operatetype == "输入_id":
+                                FunctionAutomation().operateInput(operatetype, element, driver, caseid, parameter)
+                                time.sleep(waittime)
+                            elif operatetype == "输入_xpath":
+                                FunctionAutomation().operateInput(operatetype, element, driver, caseid, parameter)
+                                time.sleep(waittime)
+                            elif operatetype == "输入_classid":
+                                FunctionAutomation().operateInput(operatetype, element, driver, caseid, parameter)
+                                time.sleep(waittime)
+                            elif operatetype == "输入_textname":
+                                FunctionAutomation().operateInput(operatetype, element, driver, caseid, parameter)
+                                time.sleep(waittime)
+                            elif operatetype == "物理按钮":
+                                FunctionAutomation().operatePhysicsKye(driver, caseid, int(parameter))
+                                time.sleep(waittime)
+                            elif operatetype == "点击_确认":
+                                FunctionAutomation().operatePhysicsKye(driver, caseid, 66)
+                                time.sleep(waittime)
+                            elif operatetype == "点击_返回":
+                                FunctionAutomation().operatePhysicsKye(driver, caseid, 4)
+                                time.sleep(waittime)
+                            elif operatetype == "查找_id":
+                                FunctionAutomation().operateCheckelement(operatetype, element, driver, caseid)
+                                time.sleep(waittime)
+                            elif operatetype == "查找_xpath":
+                                FunctionAutomation().operateCheckelement(operatetype, element, driver, caseid)
+                                time.sleep(waittime)
+                            elif operatetype == "查找_classid":
+                                FunctionAutomation().operateCheckelement(operatetype, element, driver, caseid)
+                                time.sleep(waittime)
+                            elif operatetype == "查找_textname":
+                                FunctionAutomation().operateCheckelement(operatetype, element, driver, caseid)
+                                time.sleep(waittime)
+                            elif "if" in operatetype:
+                                if operatetype == "if包含_id":
+                                    executeresult = FunctionAutomation().operateCheckelement(operatetype, element,
+                                                                                             driver, caseid)
+                                    if "执行通过" in executeresult:
+                                        print(executeresult)
+                                    else:
+                                        print(executeresult)
+                                        # try:
+                                        #     x = endcasenumber[ifnumber]
+                                        # except IndexError:
+                                        #     if len(endcasenumber) - 1 >= 0:
+                                        #         x = endcasenumber[len(endcasenumber) - 1]
+                                        #         print("当前用例中的if和and不等，请检查用例")
+                                        #     else:
+                                        #         print("当前用例中的if和and不等，请检查用例")
+                                        #         pass
+                                        if len(endcasenumber) == len(casenumber):
+                                                x = endcasenumber[ifnumber]
+                                        else:
+                                            print("当前用例中的if和and不等，请检查用例")
+                                            x = endcasenumber[-1]
+                                elif "if包含_xpath":
+                                    executeresult = FunctionAutomation().operateCheckelement(operatetype, element,
+                                                                                             driver, caseid)
+                                    if "执行通过" in executeresult:
+                                        print(executeresult)
+                                    else:
+                                        print(executeresult)
+                                        if len(endcasenumber) == len(casenumber):
+                                                x = endcasenumber[ifnumber]
+                                        else:
+                                            print("当前用例中的if和and不等，请检查用例")
+                                            x = endcasenumber[-1]
+                                elif "if包含_classid":
+                                    executeresult = FunctionAutomation().operateCheckelement(operatetype, element,
+                                                                                             driver, caseid)
+                                    if "执行通过" in executeresult:
+                                        print(executeresult)
+                                    else:
+                                        print(executeresult)
+                                        if len(endcasenumber) == len(casenumber):
+                                                x = endcasenumber[ifnumber]
+                                        else:
+                                            print("当前用例中的if和and不等，请检查用例")
+                                            x = endcasenumber[-1]
+                                elif "if包含_textname":
+                                    executeresult = FunctionAutomation().operateCheckelement(operatetype, element,
+                                                                                             driver, caseid)
+                                    if "执行通过" in executeresult:
+                                        print(executeresult)
+                                    else:
+                                        print(executeresult)
+                                        if len(endcasenumber) == len(casenumber):
+                                            x = endcasenumber[ifnumber]
+                                        else:
+                                            print("当前用例中的if和and不等，请检查用例")
+                                            x = endcasenumber[-1]
+                                else:
+                                    print("用例编号:%s操作类型错误,该用例不执行。" % (caseid))
+                                ifnumber = ifnumber + 1
+                            elif operatetype == "end":
+                                print("用例编号:%s,执行通过。" % (caseid))
                             else:
                                 print("用例编号:%s操作类型错误,该用例不执行。" % (caseid))
                         else:
@@ -188,4 +406,5 @@ class FunctionAutomation(object):
                 print("当前系统名称输入错误，请输入正确的设备系统，Android或IOS。")
 
 
-FunctionAutomation().runTestCase()
+if __name__ == "__main__":
+    FunctionAutomation().runTestCase()
--- ApplicationPerformance/applicationfunction/testdemo.py
+++ ApplicationPerformance/applicationfunction/testdemo.py
@@ -1,20 +1,78 @@
-from appium import webdriver
 import time
-
+import ApplicationPerformance.applicationperformance.launchTime as launchTime
+from appium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 desired_caps = {}
 desired_caps['platformName'] = 'Android'
 desired_caps['platformVersion'] = '4.4.2'
-desired_caps['deviceName'] = 'emulator-5554'
-desired_caps['udid'] = 'emulator-5554'
+desired_caps['deviceName'] = 'emulator-5556'
+desired_caps['udid'] = 'emulator-5556'
 desired_caps['appPackage'] = 'com.ushaqi.zhuishushenqi'
 desired_caps['appActivity'] = 'com.ushaqi.zhuishushenqi.ui.SplashActivity'
+# desired_caps['appPackage'] = 'sogou.mobile.explorer.speed'
+# desired_caps['appActivity'] = 'sogou.mobile.explorer.NoDisplayActivity'
 
-driver = webdriver.Remote('http://localhost:4726/wd/hub', desired_caps)
-time.sleep(5)
-x = driver.get_window_size()['width']
-y = driver.get_window_size()['height']
-print(x,y)
-driver.find_element_by_id("com.ushaqi.zhuishushenqi:id/btnEntryApp").click()
-#driver.find_element_by_xpath("//android.widget.ImageView[@resource-id='com.ushaqi.zhuishushenqi:id/home_action_menu_more']").click()
-driver.find_element_by_name(u"去添加").click()
-driver.quit()
+#driver = webdriver.Remote('http://localhost:4726/wd/hub', desired_caps)
+# x = driver.get_window_size()['width']
+# y = driver.get_window_size()['height']
+# print(x, y)
+#driver.find_element_by_id("com.ushaqi.zhuishushenqi:id/btnEntryApp").click()
+# driver.find_element_by_xpath("//android.widget.ImageView[@resource-id='com.ushaqi.zhuishushenqi:id/home_action_menu_more']").click()
+# try:
+#     driver.find_element_by_id("com.ushaqi.zhuishushenqi:id/btnEntryApp").click()
+#     driver.find_element_by_id("com.ushaqi.zhuishushenqi:id/home_action_menu_search").click()
+#     #driver.find_elements_by_class_name("android.widget.EditText")[0].send_keys("123")
+#     #print(driver.find_elements_by_class_name("android.widget.TextView"))
+#     driver.find_elements_by_name("书名、作者、分类")[0].send_keys("12312")
+#     driver.quit()
+# except NoSuchElementException:
+#     print("sss")
+#     driver.quit()
+#driver.find_element()
+
+# driver.find_element_by_id("com.ushaqi.zhuishushenqi:id/home_action_menu_search").click()
+casedata = launchTime.ReadExcel().readeExcelData('funcase')
+endnumber = []
+number =[]
+for x in range(1, casedata.get('caserows')):  # Excel中的测试用例数据，使用for遍历每一行的数据，进行判断执行对应的操作
+    excelcasedata = casedata.get('excledata_sheel').row_values(
+        x)
+    operatetype = excelcasedata[1]
+    if operatetype == "if":
+        number.append(x)
+    if operatetype == "end":
+        endnumber.append(x)
+i = 1
+ifnumber = 0
+while i < casedata.get('caserows'):  # Excel中的测试用例数据，使用for遍历每一行的数据，进行判断执行对应的操作
+    #print(i)
+    excelcasedata = casedata.get('excledata_sheel').row_values(
+        i)
+    caseid = int(excelcasedata[0])  # 用例编号
+    operatetype = excelcasedata[1]  # 操作类型
+    element = excelcasedata[2]  # 元素属性
+    parameter = excelcasedata[3]  # 参数（如：输入的数据）
+    checkpoint = excelcasedata[4]  # 检查点对比
+    # if operatetype == "if_":
+    #     number = i
+    # if operatetype == "end":
+    #     endnumber = i
+    i = i + 1
+    if excelcasedata[5] == "":
+        waittime = 2
+    if "if包含_" in operatetype:
+        if "com" in element:
+            print(i, excelcasedata)
+        else:
+            print("执行失败")
+            try:
+                i = endnumber[ifnumber]
+            except IndexError:
+                pass
+        ifnumber = ifnumber + 1
+    elif operatetype == "end":
+        print(i,"end")
+    elif operatetype == "点击_textname":
+        print(i,excelcasedata)
+    elif operatetype == "物理按钮":
+        print(i,excelcasedata)
