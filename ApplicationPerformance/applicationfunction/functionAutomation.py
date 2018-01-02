35ca030ace8bf3ced1666408b83bb0240026dcc2
steel <allenyao224@qq.com>
2018-01-02 18:35:12 +0800

    数据写入数据库

--- ApplicationPerformance/applicationfunction/functionAutomation.py
+++ ApplicationPerformance/applicationfunction/functionAutomation.py
@@ -1,5 +1,5 @@
-# import applicationperformance.launchTime as launchTime #MAC
-import ApplicationPerformance.applicationperformance.launchTime as launchTime  # Windows
+import applicationperformance.launchTime as launchTime  # MAC
+# import ApplicationPerformance.applicationperformance.launchTime as launchTime  # Windows
 import time
 
 from appium import webdriver
@@ -20,6 +20,10 @@ class FunctionAutomation(object):
         devicesinfo['unicodeKeyboard'] = "True"
         devicesinfo['resetKeyboard'] = "True"
         return devicesinfo
+
+    # 断开APP连接
+    def stopConnectAppium(self):
+        pass
 
     # 点击事件
     def operateClick(self, operatetype, element, driver, caseid, *parameter):
@@ -89,52 +93,52 @@ class FunctionAutomation(object):
             try:
                 driver.find_element_by_id(element)
                 print("用例编号:%s,执行通过。" % (caseid))
-            except NoSuchElementException:
+            except:
                 print("用例编号:%s,执行不通过。" % (caseid))
         elif operatetype == "查找_xpath":
             try:
                 driver.find_element_by_xpath(element)
                 print("用例编号:%s,执行通过。" % (caseid))
-            except NoSuchElementException:
+            except:
                 print("用例编号:%s,执行不通过。" % (caseid))
         elif operatetype == "查找_classid":
             try:
                 driver.find_elements_by_class_name(element)
                 print("用例编号:%s,执行通过。" % (caseid))
-            except NoSuchElementException:
+            except:
                 print("用例编号:%s,执行不通过。" % (caseid))
         elif operatetype == "查找_textname":
             try:
-                driver.find_elements_by_name(element)[0]
+                driver.find_elements_by_name(element)[0]  # 注意 此方法在appium高版本上 可能无法运行
                 print("用例编号:%s,执行通过。" % (caseid))
-            except NoSuchElementException:
+            except:
                 print("用例编号:%s,执行不通过。" % (caseid))
         elif operatetype == "if包含_id":
             try:
                 driver.find_element_by_id(element)
                 return ("用例编号:%s,执行通过。" % (caseid))
-            except NoSuchElementException:
+            except:
                 return ("用例编号:%s,执行不通过。" % (caseid))
         elif operatetype == "if包含_xpath":
             try:
                 driver.find_element_by_xpath(element)
-                return("用例编号:%s,执行通过。" % (caseid))
-            except NoSuchElementException:
-                return("用例编号:%s,执行不通过。" % (caseid))
+                return ("用例编号:%s,执行通过。" % (caseid))
+            except:
+                return ("用例编号:%s,执行不通过。" % (caseid))
         elif operatetype == "if包含_classid":
             try:
                 driver.find_elements_by_class_name(element)
-                return("用例编号:%s,执行通过。" % (caseid))
-            except NoSuchElementException:
-                return("用例编号:%s,执行不通过。" % (caseid))
+                return ("用例编号:%s,执行通过。" % (caseid))
+            except:
+                return ("用例编号:%s,执行不通过。" % (caseid))
         elif operatetype == "if包含_textname":
             try:
                 driver.find_elements_by_name(element)[0]
-                return("用例编号:%s,执行通过。" % (caseid))
-            except NoSuchElementException:
-                return("用例编号:%s,执行不通过。" % (caseid))
+                return ("用例编号:%s,执行通过。" % (caseid))
+            except:
+                return ("用例编号:%s,执行不通过。" % (caseid))
         else:
-            return("用例编号:%s,执行不通过，该用例的元素属性可能有问题，请检查该用例。" % (caseid))
+            return ("用例编号:%s,执行不通过，该用例的元素属性可能有问题，请检查该用例。" % (caseid))
 
     # 拖拽操作
     def operateDrag(self):
@@ -212,7 +216,7 @@ class FunctionAutomation(object):
             print("用例编号:%s,执行不通过。" % (caseid))
 
     # 运行用例
-    def runTestCase(self):
+    def runTestCase(self, isquit):
         deviceinfo = launchTime.ReadExcel().readeExcelData('deviceinfo')
         for i in range(1, deviceinfo.get('caserows')):
             devicescase = deviceinfo.get('excledata_sheel').row_values(i)
@@ -223,25 +227,30 @@ class FunctionAutomation(object):
             appPackage = devicescase[3]  # 测试APP的包名
             appActivity = devicescase[4]  # 测试AppActivity
             port = int(devicescase[5])  # 连接Appium端口号
-            devicesinfo = FunctionAutomation().originalDevices(deviceName, platformName, platformVersion,
-                                                               appPackage, appActivity, udid)  # 连接测试设备
-            casedata = launchTime.ReadExcel().readeExcelData('funcase')  # 读取自动化用例数据
-            endcasenumber = []
-            casenumber = []
-            for j in range(1, casedata.get('caserows')):  # Excel中的测试用例数据，使用for遍历每一行的数据，进行判断执行对应的操作
-                excelcasedata = casedata.get('excledata_sheel').row_values(
-                    j)
-                operatetype = excelcasedata[1]
-                if "if" in operatetype:
-                    casenumber.append(j)
-                if "end" in operatetype:
-                    endcasenumber.append(j)
-            if platformName == "Android":
-                try:
+            devicesexecute = devicescase[7]  # 执行状态
+            devicesinfos = "设备名：" + str(deviceName) + "," + "系统版本信息：" + str(platformName) + str(platformVersion)
+            eventid = time.strftime('%Y%m%d%H%M%S', time.localtime())
+            if "Y" in devicesexecute:
+                devicesinfo = FunctionAutomation().originalDevices(deviceName, platformName, platformVersion,
+                                                                   appPackage, appActivity, udid)  # 连接测试设备
+                casedata = launchTime.ReadExcel().readeExcelData('funcase')  # 读取自动化用例数据
+                endcasenumber = []
+                casenumber = []
+                for j in range(1, casedata.get('caserows')):  # Excel中的测试用例数据，使用for遍历每一行的数据，进行判断执行对应的操作
+                    excelcasedata = casedata.get('excledata_sheel').row_values(
+                        j)
+                    operatetype = excelcasedata[1]
+                    if "if" in operatetype:
+                        casenumber.append(j)
+                    if "end" in operatetype:
+                        endcasenumber.append(j)
+                if platformName == "Android":
+                    # try:
                     driver = webdriver.Remote('http://localhost:%s/wd/hub' % (port), devicesinfo)  # 连接Appium
                     startcasetime = time.time()  # 开始执行用例时间
                     x = 1
                     ifnumber = 0
+                    startonecasetime = time.time()
                     while x < casedata.get('caserows'):  # Excel中的测试用例数据，使用for遍历每一行的数据，进行判断执行对应的操作
                         excelcasedata = casedata.get('excledata_sheel').row_values(
                             x)
@@ -251,11 +260,13 @@ class FunctionAutomation(object):
                         element = excelcasedata[2]  # 元素属性
                         parameter = excelcasedata[3]  # 参数（如：输入的数据）x
                         checkpoint = excelcasedata[4]  # 检查点对比
+                        rundescribe = excelcasedata[6]
+                        caseexecute = excelcasedata[7]
                         if excelcasedata[5] == "":
                             waittime = 2
                         else:
                             waittime = int(excelcasedata[5])  # 等待时间
-                        if "Y" in excelcasedata[7]:
+                        if "Y" in caseexecute:
                             if operatetype == "等待时间":
                                 time.sleep(waittime)
                                 print("用例编号:%s,执行通过。" % (caseid))
@@ -345,7 +356,7 @@ class FunctionAutomation(object):
                                         #         print("当前用例中的if和and不等，请检查用例")
                                         #         pass
                                         if len(endcasenumber) == len(casenumber):
-                                                x = endcasenumber[ifnumber]
+                                            x = endcasenumber[ifnumber]
                                         else:
                                             print("当前用例中的if和and不等，请检查用例")
                                             x = endcasenumber[-1]
@@ -357,7 +368,7 @@ class FunctionAutomation(object):
                                     else:
                                         print(executeresult)
                                         if len(endcasenumber) == len(casenumber):
-                                                x = endcasenumber[ifnumber]
+                                            x = endcasenumber[ifnumber]
                                         else:
                                             print("当前用例中的if和and不等，请检查用例")
                                             x = endcasenumber[-1]
@@ -369,7 +380,7 @@ class FunctionAutomation(object):
                                     else:
                                         print(executeresult)
                                         if len(endcasenumber) == len(casenumber):
-                                                x = endcasenumber[ifnumber]
+                                            x = endcasenumber[ifnumber]
                                         else:
                                             print("当前用例中的if和and不等，请检查用例")
                                             x = endcasenumber[-1]
@@ -394,17 +405,39 @@ class FunctionAutomation(object):
                                 print("用例编号:%s操作类型错误,该用例不执行。" % (caseid))
                         else:
                             print("用例编号:%s,执行状态为No,故不执行。" % (caseid))
-                    driver.quit()  # 关闭Appium的连接
+                        endonecasetime = time.time()
+                        runonecasetime = round(endonecasetime - startonecasetime, 2)
+                        savedata = "insert into automation_function_app  (`devicesinfos`,`appiumport`,`devicesexecute`,`operatetype`,`element`,`parameter`,`waittime`,`rundescribe`,`caseexecute`,`runcasetime`,`caseid`,`eventid`,`createdtime`,`updatetime`)VALUES('%s','%s','%s','%s',\"%s\",'%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (
+                            devicesinfos, port, devicesexecute, operatetype, element, parameter, waittime, rundescribe,
+                            caseexecute,
+                            runonecasetime, caseid, eventid,
+                            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                        launchTime.MysqlConnect().saveDatatoMysql("%s" % (savedata))
+                    if isquit == "open":
+                        pass
+                    else:
+                        driver.quit()  # 关闭Appium的连接
                     endtimecasetime = time.time()  # 结束用例执行时间
-                    print("执行用例时间为:%ss" % (round(endtimecasetime - startcasetime, 2)))  # 计算执行用例运行时间
-                except:
-                    driver.quit()
-                    print("请检查Appium和设备是否正常连接。")
-            elif platformName == "IOS":
-                pass
+                    runcasetime = round(endtimecasetime - startcasetime, 2)
+                    print("第%s台设备:%s,执行用例时间为:%ss" % (i, deviceName, runcasetime))  # 计算执行用例运行时间
+                    # except:
+                    # driver.quit()
+                    # print("请检查Appium和设备是否正常连接。")
+                elif platformName == "IOS":
+                    pass
+                else:
+                    print("当前系统名称输入错误，请输入正确的设备系统，Android或IOS。")
             else:
-                print("当前系统名称输入错误，请输入正确的设备系统，Android或IOS。")
+                print("设备%s,状态为不执行，故该设备上不运行用例。"%(deviceName))
+                savedata = "insert into automation_function_app  (`devicesinfos`,`appiumport`,`devicesexecute`,`operatetype`,`element`,`parameter`,`waittime`,`rundescribe`,`caseexecute`,`runcasetime`,`caseid`,`eventid`,`createdtime`,`updatetime`)VALUES('%s','%s','%s','%s',\"%s\",'%s','%s','%s','%s','%s','%s','%s','%s','%s')" % (
+                    devicesinfos, port, devicesexecute, "", "", "", "", "",
+                    "",
+                    "", "", eventid,
+                    time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+                    time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                launchTime.MysqlConnect().saveDatatoMysql("%s" % (savedata))
 
 
 if __name__ == "__main__":
-    FunctionAutomation().runTestCase()
+    FunctionAutomation().runTestCase("Noopen")
