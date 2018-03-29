0fb25772b13a1929407fbf2eca39ddafe66f3e72
鈊诺 <allenyao224@qq.com>
2018-03-30 00:55:39 +0800

    提交001

--- xinnuo/xinnuo/settings.py
+++ xinnuo/xinnuo/settings.py
@@ -170,4 +170,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') #配置静态文件的路径
